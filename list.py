d4367b6530c0d2b89dfaf11e8515d4d8a355dacd
Mary Garcia <rose.pajanel@yahoo.com>
2024-04-12 12:56:15 +1200

    add list.py file

---
+++ list.py
@@ -0,0 +1,24 @@
+# Make a list with multiple values
+
+List = ["My", "Own", "list"]
+print("Here is the List values: " )
+print(List)
+
+# Nesting list 
+List2 = [['My', 'Own'], ['List']] 
+print("\nMulti-Dimensional List: ") 
+print(List2) 
+ 
+# Access element from list using index number 
+print("Accessing element from the list") 
+print(List[0])  
+print(List[2]) 
+ 
+# Access element using negative indexing 
+print("Accessing element using negative indexing") 
+     
+# print list last element 
+print(List[-1]) 
+     
+# print list third last element  
+print(List[-3])
