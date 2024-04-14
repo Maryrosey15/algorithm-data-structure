59d7beaf70a597e045783b8cdeed624bdbb77412
Mary Garcia <rose.pajanel@yahoo.com>
2024-04-14 20:18:11 +1200

    Dictionary file added

---
+++ dictionary.py
@@ -0,0 +1,17 @@
+# Create dictionary
+dict = {'Name' : 'Joe', 1: [1,2,3,4]}
+print("Dictionary created: ")
+print(dict)
+
+my_dict = {x: x**2 for x in [1, 2, 3, 4, 5]}
+print("Dictionary comprehension created: ")
+print(my_dict)
+
+print("Access element using get fucntion from python")
+print(dict['Name'])
+
+print("Acees element using key:")
+print(dict.get(1))
+
+
+
